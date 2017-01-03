eaa7562ac02e6788adbe32fc1bd3e946e4507b62
ckckckc <refala246@gmail.com>
2017-01-03 18:05:12 +0800

    add constructor

--- src/js/client.js
+++ src/js/client.js
@@ -2,9 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class Layout extends React.Component {
+  constructor() {
+    super();
+
+    this.nameList = [
+      'Leo',
+      'John',
+      'Frank'
+    ];
+  }
+
+  getName(num) {
+    return this.nameList[num];
+  }
+
   render() {
     return (
-      <h1>It works!</h1>
+      <h1>Hello {this.getName(0)}!</h1>
     );
   }
 }
