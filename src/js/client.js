61dc23647b41357e6ff92e73456bcaadebb1efa5
ckckckc <refala246@gmail.com>
2017-01-03 18:46:32 +0800

    add component

--- src/js/client.js
+++ src/js/client.js
@@ -1,27 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class Layout extends React.Component {
-  constructor() {
-    super();
-
-    this.nameList = [
-      'Leo',
-      'John',
-      'Frank'
-    ];
-  }
-
-  getName(num) {
-    return this.nameList[num];
-  }
-
-  render() {
-    return (
-      <h1>Hello {this.getName(0)}!</h1>
-    );
-  }
-}
+import Layout from "./components/Layout.js";
 
 const app = document.getElementById('app');
 
