188b4589c940b6f2c809a8f3f2c435aa83610b7c
ckckckc <refala246@gmail.com>
2017-01-04 16:59:23 +0800

    props and state

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -3,10 +3,18 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default class Layout extends React.Component {
+  constructor() {
+    super();
+    this.state = {'name' : 'Wellcome'};
+  }
   render() {
+    setTimeout(() => {
+      this.setState({'name' : 'Wellcome Leo'});
+    }, 1500);
+
     return (
       <div>
-        <Header />
+        <Header name={this.state.name} />
         <Footer />
       </div>
     );
